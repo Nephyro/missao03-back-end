5ae0f19ce804253859d0865b9fc43f79ca33f0ea
Nephyro <anderson.soares.4245@gmail.com>
2026-03-03 19:50:58 -0300

    atualizando exercicio 2

--- exercicio 02/app.js
+++ exercicio 02/app.js
@@ -17,3 +17,156 @@ console.log(`\n*******************************************
              \n Boas Vindas ao Sistema Calcular de médias
              \n*******************************************\n`)
 
+entradaDeDados.question('Qual o gênero do aluno(a)? \nFeminino ou Masculino: ', function(generoAluno){
+    let sexoAluno = generoAluno.trim().toLowerCase()
+
+    entradaDeDados.question('Qual o gênero do professor(a)? \nFeminino ou Masculino: ', function(generoProfessor){
+        let sexoProfessor = generoProfessor.trim().toLowerCase()
+
+        entradaDeDados.question('Digite o nome do professor(a): ', function(nomeDoProfessor){
+            let nomeProfessor = nomeDoProfessor
+
+            entradaDeDados.question('Digite o nome do aluno(a): ', function(nomedoAluno){
+                let nomeAluno = nomedoAluno
+
+                entradaDeDados.question('Digite o curso que o aluno(a) está: ', function(curso){
+                    let cursoAluno = curso
+
+                    entradaDeDados.question('Digite a disciplina do curso: ', function(disciplina){
+                        let disciplinaCurso = disciplina
+
+                        entradaDeDados.question('Digite a nota 1: ', function(valor1){
+                            let nota1 = Number(valor1)
+
+                            entradaDeDados.question('Digite a nota 2: ', function(valor2){
+                                let nota2 = Number(valor2)
+
+                                entradaDeDados.question('Digite a nota 3: ', function(valor3){
+                                    let nota3 = Number(valor3)
+
+                                    entradaDeDados.question('Digite a nota 4: ', function(valor4){
+                                        let nota4 = Number(valor4)
+
+                                    let validar = sistem.tratrativas(nota1, nota2, nota3, nota4, nomeAluno, nomeProfessor, sexoAluno, sexoProfessor, cursoAluno, disciplinaCurso)
+                                    let media = sistem.mediaAluno(nota1, nota2, nota3, nota4)
+                                    let statusAluno = sistem.situacaoAluno(nota1, nota2, nota3, nota4)
+                                    let validarGenero = sistem.validacaoGenero(sexoAluno, sexoProfessor)
+
+                                    if(media.situacaoAluno == 'APROVADO!' || validar !== false){
+                                        console.log(`\n${validarGenero.aluno}: ${nomeAluno} foi ${statusAluno} na disciplina ${disciplinaCurso}
+                                                    \nCurso: ${cursoAluno}
+                                                    \n${validarGenero.professor}: ${nomeProfessor}
+                                                    \nMédia final: ${media.toFixed(2)}\n`)
+                                                entradaDeDados.close()
+                                    }else if(media.situacaoAluno == 'REPROVADO!'){
+                                        console.log(`\n${validarGenero.aluno}: ${nomeAluno} foi ${statusAluno} na disciplina ${disciplinaCurso}
+                                                    \nCurso: ${cursoAluno}
+                                                    \n${validarGenero.professor}: ${nomeProfessor}
+                                                    \nMédia final: ${media.toFixed(2)}\n`)
+                                    }else if(media.sexoAluno == 'EXAME!'){
+                                        entradaDeDados.question('Digite a nota do exame: ', function(notaExtra){
+                                            let notaExame = Number(notaExtra)
+
+                                            let mediaFinalAluno = sistem.mediaFinal(nota1, nota2, nota3, nota4, notaExame)
+
+                                            let mediaExame = sistem.mediaFinal(media, notaExame)
+
+                                            
+
+                                        })
+                                    }
+
+
+
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+
+// if(['feminino', 'feminina'].includes(sexoAluno)){
+        //     entradaDeDados.question('Digite o nome da aluna: ', function(identidadeAluno){
+        //         let nomeAluno = identidadeAluno
+
+        //         entradaDeDados.question('Digite a nota 1: ', function(valor1){
+        //             let nota1 = valor1
+
+        //             entradaDeDados.question('Digite a nota 2: ', function(valor2){
+        //                 let nota2 = valor2
+
+        //                 entradaDeDados.question('Digite a nota 3: ', function(valor3){
+        //                     let nota3 = valor3
+
+        //                     entradaDeDados.question('Digite a nota 4: ', function(valor4){
+        //                         let nota4 = valor4
+
+
+        //                         let verificacao = sistem.tratrativas(nota1, nota2, nota3, nota4, nomeAluno, sexoAluno)
+        //                         let calculoMedia = sistem.mediaAluno(nota1, nota2, nota3, nota4)
+        //                         let situacaoDoAluno = sistem.situacaoAluno(nota1, nota2, nota3, nota4)
+
+        //                         if(verificacao !== false){
+        //                             console.log('ERRO! Informações inválidas')
+        //                         }else{
+
+
+        //                             if(['feminino', 'feminina'].includes(sexoProfessor)){
+        //                                 let
+        //                             }else if(['masculino', 'masculina'].includes(sexoProfessor)){
+        //                                 return 'Professor'
+        //                             }
+
+                                    
+        //                             console.log(`\nA aluna ${nomeAluno} foi ${situacaoDoAluno} na disciplina **** 
+        //                                          \nCurso: Desenvolvimento de Sistemas
+        //                                          \n${escolhaProfessor}
+        //                                          \nMédia Final: ${calculoMedia.toFixed(2)}\n`)
+        //                             entradaDeDados.close()
+        //                         }
+        //                     })
+        //                 })
+        //             })
+        //         })
+        //     })
+        // }else if(['masculino', 'masculina'].includes(sexoAluno)){
+        //     entradaDeDados.question('Digite o nome do aluno: ', function(identidadeAluno){
+        //         let nomeAluno = identidadeAluno
+
+        //         entradaDeDados.question('Digite a nota 1: ', function(valor1){
+        //             let nota1 = valor1
+
+        //             entradaDeDados.question('Digite a nota 2: ', function(valor2){
+        //                 let nota2 = valor2
+
+        //                 entradaDeDados.question('Digite a nota 3: ', function(valor3){
+        //                     let nota3 = valor3
+
+        //                     entradaDeDados.question('Digite a nota 4: ', function(valor4){
+        //                         let nota4 = valor4
+
+
+        //                         let verificacao = sistem.tratrativas(nota1, nota2, nota3, nota4, nomeAluno, sexoAluno)
+        //                         let calculoMedia = sistem.mediaAluno(nota1, nota2, nota3, nota4)
+        //                         let situacaoDoAluno = sistem.situacaoAluno(nota1, nota2, nota3, nota4)
+
+        //                         if(verificacao !== false){
+        //                             console.log('ERRO! Informações inválidas')
+        //                         }else{
+        //                             console.log(`\nO aluno ${nomeAluno} foi ${situacaoDoAluno} na disciplina **** 
+        //                                          \nCurso: Desenvolvimento de Sistemas
+        //                                          \n${escolhaProfessor}
+        //                                          \nMédia Final: ${calculoMedia.toFixed(2)}\n`)
+        //                             entradaDeDados.close()
+        //                         }
+        //                     })
+        //                 })
+        //             })
+        //         })
+                
+        //     })
+        // }
--- exercicio 02/modulo/sistema.js
+++ exercicio 02/modulo/sistema.js
@@ -5,21 +5,27 @@
  * Versão: 1.0 
  ******************************************************************************************************************/
 
-const tratrativas = function(nota1, nota2, nota3, nota4, nomeAluno, nomeProfessor, sexoAluno, sexoProfessor, notaExame){
+const tratrativas = function(nota1, nota2, nota3, nota4, nomeAluno, nomeProfessor, sexoAluno, sexoProfessor, cursoAluno, disciplinaCurso, notaExame){
     let valor1 = Number(String(nota1).replace(',', '.'))
     let valor2 = Number(String(nota2).replace(',', '.'))
     let valor3 = Number(String(nota3).replace(',', '.'))
     let valor4 = Number(String(nota4).replace(',', '.'))
+
+    let validacaoGenero = ['feminino', 'feminina', 'masculino', 'masculina']
+
     let nomeDoAluno = nomeAluno
     let nomeDoProfessor = nomeProfessor
     let sexo1 = sexoAluno
     let sexo2 = sexoProfessor
     let notaAdicional = Number(String(notaExame).replace(',', '.'))
+    let curso = cursoAluno
+    let disciplina = disciplinaCurso
 
     if(isNaN(valor1) || valor1 == '' || valor1 < 0 || valor1 > 100 || isNaN(valor2) || valor2 == '' || valor2 < 0 || 
-        valor2 > 100 || isNaN(valor3) || valor3 == '' || valor3 < 0 || valor3 > 100 || isNaN(valor4) || valor4 == '' || 
-        valor4 < 0 || valor4 > 100 || !isNaN(nomeDoAluno) || nomeDoAluno == '' || !isNaN(nomeDoProfessor) || 
-        nomeDoProfessor == '' || !isNaN(sexo1) || sexo1 == '' || !isNaN(sexo2) || sexo2 == '' || isNaN(notaAdicional) ||notaAdicional == ''){
+        valor2 > 100 || isNaN(valor3) || valor3 == '' || valor3 < 0 || valor3 > 100 || isNaN(valor4) || valor4 == '' || valor4 < 0 || 
+        valor4 > 100 || !isNaN(nomeDoAluno) || nomeDoAluno == '' || !isNaN(nomeDoProfessor) || !isNaN(curso) || curso == '' || !isNaN(disciplina) ||
+        disciplina == '' || nomeDoProfessor == '' || !isNaN(sexo1) || sexo1 == '' || !isNaN(sexo2) || sexo2 == '' || isNaN(notaAdicional) ||
+        notaAdicional == '' || !validacaoGenero.includes(sexo1.toLowerCase()) || !validacaoGenero.includes(sexo2.toLowerCase())){
             return false
         }else{
             return true
@@ -32,7 +38,7 @@ const mediaAluno = function(nota1, nota2, nota3, nota4){
     let valor3 = Number(nota3)
     let valor4 = Number(nota4)
     
-    let media = valor1 * valor2 * valor3 * valor4 / 4
+    let media = (valor1 + valor2 + valor3 + valor4) / 4
     
     return media
 }
@@ -52,15 +58,15 @@ const situacaoAluno = function(nota1, nota2, nota3, nota4){
     return situacao
 }
 
-const mediaFinal = function(notaExame){
-    let situacao    = situacaoAluno(nota1, nota2, nota3, nota4)
-    let media       = mediaAluno(nota1, nota2, nota3, nota4)
-    let notaAdicional = Number(String(notaExame).replace(',', '.'))
+const mediaFinal = function(media, notaExame){
+    let situacao        = situacaoAluno(nota1, nota2, nota3, nota4)
+    let mediaDoAluno    = mediaAluno(nota1, nota2, nota3, nota4)
+    let notaAdicional   = Number(String(notaExame).replace(',', '.'))
     let situacaoFinal
     let situacaoDasituacao
 
     if(situacao == 'EXAME!'){
-        situacaoFinal = media + notaAdicional
+        situacaoFinal = (mediaDoAluno + notaAdicional) / 2
 
         if(situacaoFinal > 60){
             situacaoDasituacao = 'APROVADO NO EXAME!'
@@ -72,9 +78,33 @@ const mediaFinal = function(notaExame){
     return situacaoDasituacao
 }
 
+const validacaoGenero = function(sexoAluno, sexoProfessor){
+    
+
+    let genero = {
+        aluno: 'O aluno',
+        professor: 'Professor',
+        status: situacaoAluno
+    }
+
+    if(['feminina', 'feminino'].includes(sexoAluno)){
+        genero.aluno = 'A aluna'
+
+        if(mediaAluno === 'APROVADO!') genero.status = 'APROVADA!'
+        if(mediaAluno === 'REPROVADO!') genero.status = 'REPROVADA!'
+        }
+
+    if(['feminina', 'feminino'].includes(sexoProfessor)){
+        genero.professor = 'Professora'
+    }
+    
+    return genero
+}
+
 module.exports = {
     tratrativas,
     mediaAluno,
     situacaoAluno,
-    mediaFinal
+    mediaFinal,
+    validacaoGenero
 }
